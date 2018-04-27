27ea6e969b10a521398fc2852244e45f409e7750
Odinlevi <odinlevi@gmail.com>
2018-04-27 22:08:34 +0300

    27.04

--- Finder/Avito_Youla_.py
+++ Finder/Avito_Youla_.py
@@ -6,10 +6,12 @@ import smtplib
 import time
 from time import sleep
 
+# Here are emails, which are need for script's work
 sender = 'maximiliannikiforov@mail.ru'
-#destination = 'maximiliannikiforov@gmail.com'
-destination = 'Zenonius@gmail.com'
+destination = 'maximiliannikiforov@gmail.com'
+#destination = 'Zenonius@gmail.com'
 
+# Here is beginnig of work with files and all the instructions
 try:
     file = open('hrefs.txt', 'r')
     file.close
@@ -25,11 +27,18 @@ except:
           '!!!! Every tag must be splitted by "\\n"\n'
           'Example: IPhone 6S\n'
           'Intel Core I7\n'
-          'Mercedes benz C 300 4MATIC')
+          'Mercedes benz C 300 4MATIC\n'
+          'You can also use such signs like @ and $\n'
+          'First is used for region\'s denomination: 1 or 2 (Moscow or Moscow Region)\n'
+          'Second is used for definition of price limits in rubles\n'
+          'You can type 1000 (max price) or 500:1000 (min and max prices)')
     sys.exit()
+# I found the file tags.txt, so shall we begin
 if tags != ['']:
+    # Avito.ru
     for finder in tags:
         part1 = 'https://www.avito.ru'
+        # If you typed the sign @, I'll change the region
         if finder.find('@') != -1:
             semn = finder.find('@')
             if finder[semn+1] == '1':
@@ -39,41 +48,32 @@ if tags != ['']:
             finder = finder.replace(finder[semn]+finder[semn+1], '')
         else:
             region = 'rossiya'
-        if finder.find('$') != -1:
-            if finder.find(':') != -1:
-                price_min = int(finder[finder.find('$')+1 : finder.find(':')])
-                price_max = int(finder[finder.find(':')+1 : ])
-            else:
-                price_max = int(finder[finder.find('$')+1 : ])
-                price_min = 0
-            finder = finder[:-finder.find('$')]
+        # If you typed the sign $, I'll change the price limits
+        if finder.find('$') == -1:
+            lenta = 'https://www.avito.ru/'+region+'?q='+finder
         else:
-            price_min = 0
-            price_max = 0
-        #print(price_min, ' ', price_max)
-        #print(finder)
-        lenta = 'https://www.avito.ru/'+region+'?q='+finder
+            if finder.find(':') != -1:
+                lenta='https://www.avito.ru/'+region+'?pmin='+finder[finder.find('$')+1 : finder.find(':')]+'&pmax='+finder[finder.find(':')+1 : ]+'&q='+finder[:finder.find('$')]
+            else:
+                lenta='https://www.avito.ru/'+region+'?pmax='+finder[finder.find('$')+1 : ]+'&q='+finder[:finder.find('$')]
+            finder = finder[:-finder.find('$')]
+        # Beginning of work with the site www.Avito.ru
         page = urllib.request.urlopen(lenta)
         soup = BeautifulSoup(page.read(), "html.parser")
         etc = soup.find_all("a", class_="item-description-title-link")
+        # Here I got all the links, and I just to use them right
         for hrefs in etc:
+            # Link is ready!
             url = part1 + hrefs.get('href')
-            if price_max != 0:
-                price_txt = hrefs.findNext("div", class_="about ").get_text().replace('руб.', '').replace('Цена не указана', '0').replace('\n', '').replace(' ', '')
-                if price_min != 0:
-                    if price_min >= int(price_txt) or price_max <= int(price_txt):
-                        continue
-                else:
-                    if price_max <= int(price_txt):
-                        continue
-            #print(url)
             f_r = open('hrefs.txt', 'r')
             f_cont = f_r.read()
             f_r.close()
             if f_cont.find(url) == -1:
+                # If this is a new link, I'll send you  an email
                 smtpObj = smtplib.SMTP('smtp.mail.ru', 587)
                 smtpObj.ehlo()
                 smtpObj.starttls()
+                # Yeah, it's my password
                 smtpObj.login(sender,'nelyublyuparoli')
                 msg = "\r\n".join([
                   "From: "+sender,
@@ -87,35 +87,45 @@ if tags != ['']:
                 f_w = open('hrefs.txt', 'a')
                 f_w.write(url+'\n')
                 f_w.close()
+                # Nice!
                 print("Sended: "+url)
+                #break
                 time.sleep(30)
+    # Youla.ru
     for finder in tags:
         part1 = 'https://youla.ru'
+        # If you typed the sign @, I'll change the region
+        region = ''
         if finder.find('@') != -1:
             finder = finder.replace(finder[finder.find('@')]+finder[finder.find('@')+1], '')
+            region = 'moskva'
+        # If you typed the sign $, I'll change the price limits
         if finder.find('$') == -1:
             lenta = 'https://youla.ru/?q='+finder
         else:
             if finder.find(':') != -1:
-                lenta='https://youla.ru/?attributes[price][from]='+finder[finder.find('$')+1 : finder.find(':')]+'00&attributes[price][to]='+finder[finder.find(':')+1 : ]+'00&q='+finder[:finder.find('$')]
+                lenta='https://youla.ru/'+region+'?attributes[price][from]='+finder[finder.find('$')+1 : finder.find(':')]+'00&attributes[price][to]='+finder[finder.find(':')+1 : ]+'00&q='+finder[:finder.find('$')]
             else:
-                lenta='https://youla.ru/?attributes[price][to]='+finder[finder.find('$')+1 : ]+'00&q='+finder[:finder.find('$')]
+                lenta='https://youla.ru/'+region+'?attributes[price][to]='+finder[finder.find('$')+1 : ]+'00&q='+finder[:finder.find('$')]
             finder = finder[:-finder.find('$')]
-        #print(lenta)
+        # Beginning of work with the site www.Youla.ru
         page = urllib.request.urlopen(lenta)
         soup = BeautifulSoup(page.read(), "html.parser")
         f_li = soup.find_all("li", class_="product_item")
+        # Here I got all the links, and I just to use them right
         for hrefs in f_li:
             etc = hrefs.find_next("a")
+            # Link is ready!
             url = part1 + etc.get('href')
-            #print(url)
             f_r = open('hrefs.txt', 'r')
             f_cont = f_r.read()
             f_r.close()
             if f_cont.find(url) == -1:
+                # If this is a new link, I'll send you  an email
                 smtpObj = smtplib.SMTP('smtp.mail.ru', 587)
                 smtpObj.ehlo()
                 smtpObj.starttls()
+                # Yeah, it is the same... Or not?
                 smtpObj.login(sender,'nelyublyuparoli')
                 msg = "\r\n".join([
                   "From: "+sender,
@@ -130,4 +140,5 @@ if tags != ['']:
                 f_w.write(url+'\n')
                 f_w.close()
                 print("Sended: "+url)
+                # Nice!
                 time.sleep(30)

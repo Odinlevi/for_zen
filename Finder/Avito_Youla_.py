184fbcb1f908d1f346768e9cebf7f752cfb8bf46
Odinlevi <odinlevi@gmail.com>
2018-05-01 09:39:35 +0300

    01.05

--- Finder/Avito_Youla_.py
+++ Finder/Avito_Youla_.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import urllib.request, socket
+from urllib.parse import quote
 import requests
 import sys
 import smtplib
@@ -13,14 +14,14 @@ destination = 'maximiliannikiforov@gmail.com'
 
 # Here is beginnig of work with files and all the instructions
 try:
-    file = open('hrefs.txt', 'r')
+    file = open('hrefs.txt', 'r', encoding="utf-8")
     file.close
 except:
     print('The reference database was not created / was deleted, I create a new one...')
-    file = open('hrefs.txt', 'w')
+    file = open('hrefs.txt', 'w', encoding="utf-8")
     file.close
 try:
-    f_tags = open('tags.txt', 'r')
+    f_tags = open('tags.txt', 'r', encoding="utf-8")
     tags = f_tags.read().split('\n')
 except:
     print('Please, create file "tags.txt" and fill it with tags;\n'
@@ -50,26 +51,33 @@ if tags != ['']:
             region = 'rossiya'
         # If you typed the sign $, I'll change the price limits
         if finder.find('$') == -1:
-            lenta = 'https://www.avito.ru/'+region+'?q='+finder
+            lenta = 'https://www.avito.ru/'+region+'?q={0}&i=1'.format(quote(finder))
         else:
             if finder.find(':') != -1:
-                lenta='https://www.avito.ru/'+region+'?pmin='+finder[finder.find('$')+1 : finder.find(':')]+'&pmax='+finder[finder.find(':')+1 : ]+'&q='+finder[:finder.find('$')]
+                lenta='https://www.avito.ru/'+region+'?pmin='+finder[finder.find('$')+1 : finder.find(':')]+'&pmax='+finder[finder.find(':')+1 : ]+'&q={0}&i=1'.format(quote(finder[:finder.find('$')]))
             else:
-                lenta='https://www.avito.ru/'+region+'?pmax='+finder[finder.find('$')+1 : ]+'&q='+finder[:finder.find('$')]
-            finder = finder[:-finder.find('$')]
+                lenta='https://www.avito.ru/'+region+'?pmax='+finder[finder.find('$')+1 : ]+'&q={0}&i=1'.format(quote(finder[:finder.find('$')]))
+            finder = finder[:finder.find('$')]
         # Beginning of work with the site www.Avito.ru
-        page = urllib.request.urlopen(lenta)
+        try:
+            page = urllib.request.urlopen(lenta)
+        except:
+            print('There is no such item on Avito.ru now - '+finder)
+            continue
         soup = BeautifulSoup(page.read(), "html.parser")
         etc = soup.find_all("a", class_="item-description-title-link")
-        # Here I got all the links, and I just to use them right
+        count = 0
+        # Here I got all the links, and I just try to use them right
         for hrefs in etc:
             # Link is ready!
+            if count >= 10:
+                break
             url = part1 + hrefs.get('href')
-            f_r = open('hrefs.txt', 'r')
+            f_r = open('hrefs.txt', 'r', encoding="utf-8")
             f_cont = f_r.read()
             f_r.close()
             if f_cont.find(url) == -1:
-                # If this is a new link, I'll send you  an email
+                # If this is a new link, I'll send you an email
                 smtpObj = smtplib.SMTP('smtp.mail.ru', 587)
                 smtpObj.ehlo()
                 smtpObj.starttls()
@@ -84,13 +92,16 @@ if tags != ['']:
                   ])
                 smtpObj.sendmail(sender,destination,msg)
                 smtpObj.quit()
-                f_w = open('hrefs.txt', 'a')
+                f_w = open('hrefs.txt', 'a', encoding="utf-8")
                 f_w.write(url+'\n')
                 f_w.close()
+                count += 1
                 # Nice!
                 print("Sended: "+url)
                 #break
                 time.sleep(30)
+        if count == 0:
+            print('There is no such item on Avito.ru now - '+finder)
     # Youla.ru
     for finder in tags:
         part1 = 'https://youla.ru'
@@ -101,27 +112,30 @@ if tags != ['']:
             region = 'moskva'
         # If you typed the sign $, I'll change the price limits
         if finder.find('$') == -1:
-            lenta = 'https://youla.ru/?q='+finder
+            lenta = 'https://youla.ru/?q={0}'.format(quote(finder))
         else:
             if finder.find(':') != -1:
-                lenta='https://youla.ru/'+region+'?attributes[price][from]='+finder[finder.find('$')+1 : finder.find(':')]+'00&attributes[price][to]='+finder[finder.find(':')+1 : ]+'00&q='+finder[:finder.find('$')]
+                lenta='https://youla.ru/'+region+'?attributes[price][from]='+finder[finder.find('$')+1 : finder.find(':')]+'00&attributes[price][to]='+finder[finder.find(':')+1 : ]+'00&q={0}'.format(quote(finder[:finder.find('$')]))
             else:
-                lenta='https://youla.ru/'+region+'?attributes[price][to]='+finder[finder.find('$')+1 : ]+'00&q='+finder[:finder.find('$')]
-            finder = finder[:-finder.find('$')]
+                lenta='https://youla.ru/'+region+'?attributes[price][to]='+finder[finder.find('$')+1 : ]+'00&q={0}'.format(quote(finder[:finder.find('$')]))
+            finder = finder[:finder.find('$')]
         # Beginning of work with the site www.Youla.ru
         page = urllib.request.urlopen(lenta)
         soup = BeautifulSoup(page.read(), "html.parser")
         f_li = soup.find_all("li", class_="product_item")
-        # Here I got all the links, and I just to use them right
+        count = 0
+        # Here I got all the links, and I just try to use them right
         for hrefs in f_li:
+            if count >= 10:
+                break
             etc = hrefs.find_next("a")
             # Link is ready!
             url = part1 + etc.get('href')
-            f_r = open('hrefs.txt', 'r')
+            f_r = open('hrefs.txt', 'r', encoding="utf-8")
             f_cont = f_r.read()
             f_r.close()
             if f_cont.find(url) == -1:
-                # If this is a new link, I'll send you  an email
+                # If this is a new link, I'll send you an email
                 smtpObj = smtplib.SMTP('smtp.mail.ru', 587)
                 smtpObj.ehlo()
                 smtpObj.starttls()
@@ -136,9 +150,12 @@ if tags != ['']:
                   ])
                 smtpObj.sendmail(sender,destination,msg)
                 smtpObj.quit()
-                f_w = open('hrefs.txt', 'a')
+                f_w = open('hrefs.txt', 'a', encoding="utf-8")
                 f_w.write(url+'\n')
                 f_w.close()
+                count += 1
                 print("Sended: "+url)
                 # Nice!
                 time.sleep(30)
+        if count == 0:
+            print('There is no such item on Youla.ru now - '+finder)

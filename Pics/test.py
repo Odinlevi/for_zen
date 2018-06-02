81a6e1f1512756a5e7a899549e6cb41ed73b70bf
user00 <user00@e1p34.chisinau.42.fr>
2018-06-02 14:54:13 +0300

    02.06

--- Pics/test.py
+++ Pics/test.py
@@ -1,8 +1,40 @@
 import urllib.request
-from PIL import Image, ImageOps
+from urllib.parse import quote
+#from PIL import Image, ImageOps
 import ssl
 ssl.match_hostname = lambda cert, hostname: True
-img_href = 'https:///кфс-кольцова-центр-регион.рфwp-content/uploads/kfs-kolcova-centr-region-sait.png'.encode('idna')
+
+def check_cyrill(href):
+    try:
+        href.encode('ascii')
+        return(1)
+    except:
+        return(0)
+def http(href):
+    try:
+        check = urllib.request.urlopen('http://'+href)
+        return(1)
+    except:
+        try:
+            check = urllib.request.urlopen('https://'+href)
+            return(0)
+        except:
+            return(-1)
+
+img_hrefv1 = 'кфс-кольцова-центр-регион.рф'
+img_href = img_hrefv1
+if check_cyrill(img_href) == 0:
+    img_parts = img_href.split('.')
+    img_href = ''
+    for img_part in img_parts:
+        img_href += str(img_part.encode('punycode')).replace("b'", "xn--").replace("'", ".")
+    img_href = img_href[:-1]
+if http(img_href):
+    img_href = 'http://'+img_href
+elif http(img_href) == 0:
+    img_href = 'https://'+img_href
+else:
+    print('BAD URL: '+img_href)
 print(img_href)
 '''
 resource = urllib.request.urlopen(img_href)
